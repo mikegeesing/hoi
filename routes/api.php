7b8f6f6a1778fffaceae60e8e4313a90ca57c26d
mikegeesing <63345796+mikegeesing@users.noreply.github.com>
2025-12-17 06:18:03 +0000

    Add /api/restore route for direct restore functionality

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\TokenController;
+use App\Http\Controllers\RestoreController;
 
 Route::post('/admin/token/create', [TokenController::class, 'store']);
+Route::post('/restore', [RestoreController::class, 'startRestore']);
