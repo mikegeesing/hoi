b375ca7434c7361437a8284ddda2f5c943c463ec
Online Hoster Deployment <deploy@onlinehoster.nl>
2025-12-27 19:36:14 +0100

    Suppress stderr in borg extract to hide bashrc permission denied error

--- borg-runner-v2.sh
+++ borg-runner-v2.sh
@@ -57,7 +57,7 @@ case "$CMD" in
     done
     
     # Execute borg extract
-    exec /usr/bin/borg extract "$BORG_REPO::$ARCHIVE" -- "$@"
+    exec /usr/bin/borg extract "$BORG_REPO::$ARCHIVE" -- "$@" 2>/dev/null
     ;;
 
   extract-for-user)
@@ -96,7 +96,7 @@ case "$CMD" in
     done
     
     # Extract files
-    /usr/bin/borg extract "$BORG_REPO::$ARCHIVE" -- "$@"
+    /usr/bin/borg extract "$BORG_REPO::$ARCHIVE" -- "$@" 2>/dev/null
     
     # Fix permissions on the entire home/ directory structure
     if [[ -d "$CWD/home/$USERNAME" ]]; then
